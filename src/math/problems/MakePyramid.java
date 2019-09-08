fc177b2745d1fb3e51074693d34ab3eabd0bd9ba
farzanarashid <farzanarashid15@gmail.com>
2019-09-08 13:09:28 -0400

    math.problem-pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -13,7 +13,18 @@ public class MakePyramid {
                          * * * * * *
 
         */
+        for (int i=0; i<6; i++)
+        {
+            for (int j=6-i; j>1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i; j++ )
+            {
+                System.out.print("* ");
+            }
 
-
+            System.out.println();
         }
+    }
   }
