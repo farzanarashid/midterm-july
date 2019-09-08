4c0754b2720a910767ec6e9d1df73b30978f7518
farzanarashid <farzanarashid15@gmail.com>
2019-09-08 12:34:35 -0400

    Fibonnaci number

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,6 +6,17 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+int count=40;
+        int k1=0;int k2=1;int i=0;int k3=0;
+        System.out.println(k1+" " +k2);
+
+            for(i=2;i<count;++i)
+            {
+                k3=k1+k2;
+                System.out.print(" "+k3);
+                k1=k2;
+                k2=k3;
+            }
 
 
     }
