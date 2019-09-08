8f976f7c305495cfdb82d593007abcc3a2bf0d23
farzanarashid <farzanarashid15@gmail.com>
2019-09-08 16:17:20 -0400

    Queue

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -1,7 +1,5 @@
 package datastructure;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class CollectionView {
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,14 +1,31 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
 		/*
-		 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
+		 * emonstrate how to use Queue that includes add,peek,remove,pool elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<String> Queue = new LinkedList<>();
+		Queue.add("farhana");
+		Queue.add("farzana");
+		Queue.add("Shah");
+		Queue.add("amaya");
+		Queue.add("maruf");
+		System.out.println("Peek " + Queue.peek());
+		System.out.println("Poll " + Queue.poll());
+		System.out.println(Queue.poll());
+		System.out.println("Remove " + Queue.remove());
+		Iterator it = Queue.iterator();
+		while (it.hasNext()) {
+			System.out.println(Queue.poll());
+		}
 
 	}
-
 }
