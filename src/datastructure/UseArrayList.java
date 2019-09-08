73b1cb8bec9b3cc3817ade4fa1722fcffac5f485
farzanarashid <farzanarashid15@gmail.com>
2019-09-08 16:31:18 -0400

    Arraylist

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,7 @@
 package datastructure;
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,7 +11,24 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		List<String> Car = new ArrayList<>();
+
+		// Adding new elements to the ArrayList
+		Car.add("Toyota");
+		Car.add("Sonata");
+		Car.add("Corolla");
+		Car.add("Hynadai");
+
+		System.out.println(Car);
+		Iterator it=ArrayList.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(Car);}
+
+		Car.add(2, "Honda");
+		Car.remove(3);
+		System.out.println(Car);
+
 
 	}
 
