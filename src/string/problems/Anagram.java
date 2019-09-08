3b73f6020c51e21a45aa1d9425b1494ef594a8bd
farzanarashid <farzanarashid15@gmail.com>
2019-09-08 16:53:11 -0400

    Palindram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,4 +1,7 @@
 package string.problems;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by mrahman on 04/22/17.
@@ -8,5 +11,7 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
     }
 }
+    /* Driver program to test to print printDups*/
--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -1,11 +1,22 @@
 package string.problems;
 
 public class Palindrome {
-    public static void main(String[] args) {
-        /*
-          If a String is reversed and it remains unchanged, that is called Palindrome. For example, MOM,DAD,MADAM are
-          Palindrome. So write java code to check if a given String is Palindrome or not.
-         */
-
+    public static void main(String[] args) throws java.lang.Exception {
+        checkPalindrome("MADAM");
+        checkPalindrome("farzana");
     }
+
+    public static void checkPalindrome(String s) {
+        // reverse the given String
+        String reverse = new StringBuffer(s).reverse().toString();
+
+        // check whether the string is palindrome or not
+        if (s.equals(reverse))
+            System.out.println("The word is Palindrome");
+
+        else
+            System.out.println("The word is not Palindrome");
+    }
+
+
 }
